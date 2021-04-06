123a6ed064aea1c34a7113fcbf6c8399d78a244a
TitoRichar <TitoRichar@DESKTOP-NM4OHFP>
2021-04-06 11:42:29 +0200

    creado el main y la clase comercial con nombre y sueldo

---
+++ Rentornos/src/rentornos/Comercial.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rentornos;
+
+/**
+ *
+ * @author TitoRichar
+ */
+public class Comercial {
+    public String nombre;
+    public double sueldo;
+
+    public Comercial(String nombre, double sueldo) {
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+    }
+   
+    
+}
